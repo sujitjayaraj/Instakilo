96011ac1d20cb2d305958659ad97d7f64b001d75
Sujit Jayaraj <sujitjayaraj05@gmail.com>
2024-12-20 13:25:39 +0530

    Initial commit

---
+++ src/main/java/tech/sujitjayaraj/instakilo/InstakiloApplication.java
@@ -0,0 +1,13 @@
+package tech.sujitjayaraj.instakilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstakiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstakiloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/sujitjayaraj/instakilo/InstakiloApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.sujitjayaraj.instakilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstakiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
